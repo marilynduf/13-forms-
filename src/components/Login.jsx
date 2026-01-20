c88dea9839ec4f5805c34fbcaff026732cfac2c3
M <marilynduf@gmail.com>
2026-01-20 17:34:26 -0500

    feature: add reset form functionwith useRef

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,11 @@ export default function Login(event) {
     const email = useRef("");
     const password = useRef("");
 
+    const handleReset = function () {
+        email.current.value = "";
+        password.current.value = "";
+    };
+
     function handleSubmit(event) {
         event.preventDefault();
         console.log(email.current.value, password.current.value);
@@ -31,7 +36,7 @@ export default function Login(event) {
             </div>
 
             <p className="form-actions">
-                <button type="reset" className="button button-flat">
+                <button onClick={handleReset} className="button button-flat">
                     Reset
                 </button>
                 <button className="button">Login</button>
